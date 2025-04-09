d8799a8bc5479076d5551af00e4c5897605a7f58
HieuDo <dotrunghieu0903@gmail.com>
2025-04-09 12:16:36 -0400

    Initial Vanila Example Excercise

---
+++ script.js
@@ -0,0 +1,19 @@
+const content = [
+    [
+      "React is extremely popular",
+      "It makes building complex, interactive UIs a breeze",
+      "It's powerful & flexible",
+      "It has a very active and versatile ecosystem"
+    ],
+    [
+      "Components, JSX & Props",
+      "State",
+      "Hooks (e.g., useEffect())",
+      "Dynamic rendering"
+    ],
+    [
+      "Official web page (react.dev)",
+      "Next.js (Fullstack framework)",
+      "React Native (build native mobile apps with React)"
+    ]
+  ];
